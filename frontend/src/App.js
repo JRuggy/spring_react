f6dfb6a7b341595eb5c1c7657112cc47f1445eb6
JastonRuggy <jastonruggy@icloud.com>
2023-01-20 19:15:41 +0300

    another error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route} from 'react-router-dom';
+// import { Switch } from 'react-router';
+import { Routes, Route } from "react-router-dom";
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -7,19 +9,49 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-      <Router>
+      <div className='container'>
+        <HeaderComponent />
         <div className='container'>
-          <HeaderComponent />
-          <div className='container'>
-            <switch>
-              <Route path="/" component={ListEmployeeComponent}></Route>
-              <Route path="/employees" component={ListEmployeeComponent}></Route>
-              <ListEmployeeComponent />
-            </switch>
-          </div>
-          <FooterComponent />
+          <Routes>
+            <Route path="/" element={<ListEmployeeComponent/>} />
+            {/* <Route path='/employee' element={ListEmployeeComponent} /> */}
+          </Routes>
         </div>
-      </Router>
+        <FooterComponent />
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* <Router>
+            <Switch>
+              <Route path="/" component={ListEmployeeComponent}></Route>
+              <Route path="/employess" component={ListEmployeeComponent}></Route>
+              <ListEmployeeComponent />
+            </Switch>
+      </Router> */}
     </div>
   );
 }
